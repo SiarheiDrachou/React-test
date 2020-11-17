65257728124d4aa16df836c5ab415d6617692e7a
SiarheiDrachou <9kudza@tut.by>
2020-11-18 00:24:24 +0300

    Changed display of contact information

--- src/components/ContactInfo/Contact.jsx
+++ src/components/ContactInfo/Contact.jsx
@@ -3,19 +3,39 @@ import './Contact.scss'
 
 const Contact = props => (
     <div className="information">
-        Выбран пользователь <b>{props.userData.firstName} {props.userData.lastName}</b>
-        <br />
-            Описание:
-        <br />
-        <textarea defaultValue={props.userData.description}></textarea>
-        <br />
-            Адрес проживания: <b>{props.userData.address.streetAddress}</b>
-        <br />
-            Город: <b>{props.userData.address.city}</b>
-        <br />
-            Провинция/штат: <b>{props.userData.address.state}</b>
-        <br />
-            Индекс: <b>{props.userData.address.zip}</b>
+        {
+            props.userData.firstName === undefined || props.userData.lastName === undefined
+            ? null
+            : <span>
+                Выбран пользователь <b>{props.userData.firstName} {props.userData.lastName}</b>
+            </span>
+        }
+        
+        {
+            props.userData.description === undefined
+            ? null
+            : <span>
+                <br />
+                    Описание:
+                <br />
+                <textarea defaultValue={props.userData.description} disabled></textarea>
+            </span>
+        }
+
+        {
+            props.userData.address === undefined
+            ? null
+            : <div>
+                <br />
+                Адрес проживания: <b>{props.userData.address.streetAddress}</b>
+                <br />
+                Город: <b>{props.userData.address.city}</b>
+                <br />
+                Провинция/штат: <b>{props.userData.address.state}</b>
+                <br />
+                Индекс: <b>{props.userData.address.zip}</b>
+            </div>
+        }
     </div>
 )
 
--- src/components/Thead/TableThead.jsx
+++ src/components/Thead/TableThead.jsx
@@ -4,7 +4,6 @@ import { sortIncrease, sortDecrease } from '../../redux/actions/data';
 
 const TableThead = props => (
     <th className="th">
-        {console.log(props)}
         {props.head}
 
         <p title="Сортировка по возрастанию" style={{ display: 'inline' }}>
@@ -35,140 +34,10 @@ const TableThead = props => (
             </svg>
         </p>
     </th>
-
-            /* <th>
-                First Name
-
-                <p title="Сортировка по возрастанию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-up-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortIncrease.bind(this, "firstName") }
-                    >
-                        <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-                    </svg>
-                </p>
-
-                <p title="Сортировка по убыванию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-down-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortDecrease.bind(this, "firstName")}
-                    >
-                        <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                    </svg>
-                </p>
-            </th>
-
-            <th>
-                Last Name
-
-                <p title="Сортировка по возрастанию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-up-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortIncrease.bind(this, "lastName")}
-                    >
-                        <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-                    </svg>
-                </p>
-
-                <p title="Сортировка по убыванию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-down-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortDecrease.bind(this, "lastName")}
-                    >
-                        <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                    </svg>
-                </p>
-            </th>
-
-            <th>
-                Email
-
-                <p title="Сортировка по возрастанию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-up-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortIncrease.bind(this, "email")}
-                    >
-                        <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-                    </svg>
-                </p>
-
-                <p title="Сортировка по убыванию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-down-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortDecrease.bind(this, "email")}
-                    >
-                        <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                    </svg>
-                </p>
-            </th>
-
-            <th>
-                Phone
-
-                <p title="Сортировка по возрастанию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-up-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortIncrease.bind(this, "phone")}
-                    >
-                        <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-                    </svg>
-                </p>
-
-                <p title="Сортировка по убыванию" style={{ display: 'inline' }}>
-                    <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-caret-down-fill"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={ props.sortDecrease.bind(this, "phone")}
-                    >
-                        <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                    </svg>
-                </p>
-            </th>
-             */
 )
 
 function mapStateToProps(state) {
     return {
-        data: state.variables.data
     }
 }
 
